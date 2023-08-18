b28930dd24625a7bcfb3c1ee4eddca0f2909a458
vitorribeiro99 <v99ribeiro@gmail.com>
2023-08-18 15:17:29 +0100

    fix: color changes

--- app/src/pages/about/index.tsx
+++ app/src/pages/about/index.tsx
@@ -1,25 +1,4 @@
-import { useConnection, useWallet } from '@solana/wallet-adapter-react';
-import { LAMPORTS_PER_SOL } from '@solana/web3.js';
-import React, { useEffect } from 'react';
-
 const About = () => {
-    const wallet = useWallet();
-    const { connection } = useConnection();
-
-    let lamportBalance: any;
-
-    const getBalance = async () => {
-        if (wallet?.publicKey) {
-            const balance = await connection.getBalance(wallet.publicKey);
-            lamportBalance = balance / LAMPORTS_PER_SOL;
-            console.log(lamportBalance);
-        }
-    };
-
-    useEffect(() => {
-        getBalance();
-    }, [wallet]);
-
     return (
         <div>
             <h1>About Page</h1>
