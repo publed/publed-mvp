afd3b6cf901151a25a98fd7c439f6fd602475dcf
vitorribeiro99 <v99ribeiro@gmail.com>
2023-08-20 17:08:11 +0100

    Update index.tsx

--- app/src/pages/research/index.tsx
+++ app/src/pages/research/index.tsx
@@ -37,12 +37,18 @@ const Research = () => {
                 <div className="flex justify-between items-center mb-6">
                     <p className="text-default-50 text-xl font-medium">&lt; Back</p>
                     <div className="flex gap-4">
-                        <Pill isActive>
-                            <Document className="fill-white" /> Paper
-                        </Pill>
-                        <Pill>
-                            <Data /> Data
-                        </Pill>
+                        {researchObject?.properties?.files?.find((attr: any) => attr.type === 'application/pdf')
+                            ?.value || 'N/A' ? (
+                            <Pill isActive>
+                                <Document /> Paper
+                            </Pill>
+                        ) : null}
+                        {researchObject?.properties?.files?.find((attr: any) => attr.type === 'application/db')
+                            ?.value || 'N/A' ? (
+                            <Pill>
+                                <Data /> Data
+                            </Pill>
+                        ) : null}
                         <Pill>
                             <Video />
                             Video
@@ -59,24 +65,16 @@ const Research = () => {
                 <div className="flex justify-between gap-16">
                     <div className="max-w-lg">
                         <h2 className="text-2xl font-semibold mb-2 text-dark-blue-80">{researchObject?.description}</h2>
-                        <p className="font-medium text-lg text-dark-blue-60 mb-4">CertiK</p>
+                        {researchObject?.attributes?.find((attr: any) => attr.trait_type === 'Author')?.value || 'N/A'}
+                        <p className="font-medium text-lg text-dark-blue-60 mb-4"></p>
                         <p className="text-sm text-default-100 mb-8">
-                            <strong>DOI</strong> N/A
+                            <strong>DOI</strong>{' '}
+                            {researchObject?.attributes?.find((attr: any) => attr.trait_type === 'DOI')?.value || 'N/A'}
                         </p>
                         <h3 className="text-default-100 text-lg font-semibold mb-2">Abstract</h3>
                         <p className="text-default-100 text-sm text-justify">
-                            Virtualization technology starts becoming more and more widespread in the embedded space.
-                            The penalties incurred by standard software-based virtualization is pushing research towards
-                            hardware-assisted solutions. Among the existing commercial off-the-shelf technologies for
-                            secure virtualization, ARM TrustZone is attracting particular attention. However, it is
-                            often seen with some skepticism due to the dual-OS limitation of existing state-of-the-art
-                            solutions. This letter presents the implementation of a TrustZone-based hypervisor for
-                            real-time embedded systems, which allows multiple RTOS partitions on the same hardware
-                            platform. The results demonstrate that virtualization overhead is less than 2 percent for a
-                            10 milliseconds guest-switching rate, and the system remains deterministic. This work goes
-                            beyond related work by implementing a TrustZone-assisted solution that allows the execution
-                            of an arbitrary number of guest OSes while providing the foundation to drive next generation
-                            of secure virtualization solutions for resource-constrained embedded devices.
+                            {researchObject?.attributes?.find((attr: any) => attr.trait_type === 'Abstract')?.value ||
+                                'N/A'}
                         </p>
                     </div>
                     <div className="bg-gray-400 w-[670px] h-[448px]" />
@@ -90,7 +88,7 @@ const Pill = ({ isActive, children }: any) => {
     return (
         <div
             className={`rounded-full px-5 py-[10px] inline-flex border border-dark-blue-30 uppercase bg-white font-medium text-default-70 gap-[10px] ${
-                isActive ? 'bg-[#0088A9] text-white' : ''
+                isActive ? 'bg-[#0088A9] text-red-600' : ''
             }`}
         >
             {children}
