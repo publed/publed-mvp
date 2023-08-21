3205c58874c88a082c31981fb3fda754b9b1d304
vitorribeiro99 <v99ribeiro@gmail.com>
2023-08-21 17:26:47 +0100

    Update ResearchCard.tsx

--- app/src/components/ResearchCard.tsx
+++ app/src/components/ResearchCard.tsx
@@ -36,11 +36,11 @@ const ResearchCard = ({ description: name, attributes, properties }: ResearchCar
                 </p>
                 <div className="flex gap-4 items-center">
                     <div className="flex gap-2 fill-dark-blue-40">
-                        {properties?.files?.find((file: any) => file.type === 'application/pdf') ? <Document /> : null}
+                        {properties?.files?.find((file: any) => file.type === 'application/pdf') || name === "Cartesi - Security Analysis" ? <Document /> : null}
                         {properties?.files?.find((file: any) => file.type === 'movie/mp4') ? <Video /> : null}
                         {properties?.files?.find((file: any) => file.type === 'application/db') ? <Data /> : null}
-                        {properties?.files?.find((file: any) => file.type === 'application/pdf' || file.type === 'application/pptx') ? <Slides /> : null}
-                        {properties?.files?.find((file: any) => file.type === 'application/code') ? <Code /> : null}
+                        {properties?.files?.find((file: any) => file.type === 'application/pdf' || file.type === 'application/pptx') || name === "Cartesi - Security Analysis" ? <Slides /> : null}
+                        {properties?.files?.find((file: any) => file.type === 'application/code') || name === "BAO - A Lightweight Static Partitioning Hypervisor" || name === "Cartesi - Security Analysis" ? <Code /> : null}
                     </div>
                     {/* @ts-ignore  */}
                     <Badge variant={attributes?.find((attr: any) => attr.trait_type === 'State')?.value || 'To Review'}/>
