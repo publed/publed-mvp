5feac58e5abac24676ff64c3d202b298868b5e65
vitorribeiro99 <v99ribeiro@gmail.com>
2023-08-22 02:16:35 +0100

    review

--- app/src/pages/research/index.tsx
+++ app/src/pages/research/index.tsx
@@ -50,7 +50,7 @@ const Research = () => {
                     <button onClick={() => navigate(-1)}>
                         <p className="text-default-50 text-xl font-medium hover:font-semibold">&lt; Back</p>
                     </button>
-                    {/* <button onClick={() => navigate('/review')}>
+                    <button onClick={() => navigate('/review')}>
                         <li
                             className="flex gap-[6px] px-5 py-[10px] border rounded-full cursor-pointer 
                         bg-white text-default-70 stroke-default-70 fill-none"
@@ -60,7 +60,7 @@ const Research = () => {
                                 <path d="M18.54,9,8.88,3.46a3.42,3.42,0,0,0-5.13,3V17.58A3.42,3.42,0,0,0,7.17,21a3.43,3.43,0,0,0,1.71-.46L18.54,15a3.42,3.42,0,0,0,0-5.92Zm-1,4.19L7.88,18.81a1.44,1.44,0,0,1-1.42,0,1.42,1.42,0,0,1-.71-1.23V6.42a1.42,1.42,0,0,1,.71-1.23A1.51,1.51,0,0,1,7.17,5a1.54,1.54,0,0,1,.71.19l9.66,5.58a1.42,1.42,0,0,1,0,2.46Z"></path>
                             </svg>
                         </li>
-                    </button> */}
+                    </button>
                 </div>
                 <div className="flex justify-between gap-16">
                     <div className="max-w-lg">
